e89ffc013986acecff32fe8ea5cddd9f2a650794
bongacat07 <vaibhavanandeh07@gmail.com>
2026-08-24 16:00:15 +0530

    minlab changed

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -209,6 +209,7 @@
 					"nc doesn't do a proper 4 way teardown. that bothered me enough to write a TCP impl tester"
 				</p>
 				<p class="ch-body">
+				A simple nc implementation turned into a deterministic protocol testing tool.
 					I was using nc to test my TCP implementation. Quit the process and my impl got stuck in
 					the last ACK state. So I started with writing a nc clone with a terminate command that <span class="hl-y"
 						>completes the handshake correctly.</span
@@ -216,7 +217,7 @@
 					Then I thought, why stop there? Why not send packets at completely wrong states?
 					<span class="hl-r">SYN during ESTABLISHED. RST when the connection is half open.</span>
 					See if the implementation actually holds. So I added chaos mode. Because
-					<span class="hl-y">WHY NOT.</span>
+					<span class="hl-y">WHY NOT.</span>. Brutally test your TCP implementation.
 				</p>
 				<span class="ch-aside">hehe →</span>
 			</div>
@@ -235,11 +236,12 @@
 						<svg viewBox="0 0 24 24"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
 					</a>
 				</h3>
-				<p class="ch-why">"Kernel's where slow, but I wanted to FEEL why."</p>
+				<p class="ch-why">"Well, why does a socket need a syscall?."</p>
 				<p class="ch-body">
 					MUDP or My UDP. I wanted to know how kernels implemented networking, because <span
 						class="hl-y">you can't bypass something you truly don't understand</span
 					>
+					After understanding the sockets down to the instruction level, I wanted to know what is even a syscall like socket(), bind() even doing...
 					My own UDP implementation which is a kernel module and can be called via socket().
 				</p>
 				<span class="ch-aside">One step closer to the final boss ←</span>
